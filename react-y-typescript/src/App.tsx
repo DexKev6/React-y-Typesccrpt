6777e35d288678fff693f080f00dd3cab45ae97c
DexKev6 <kevin_saldarriaga82191@elpoli.edu.co>
2022-09-24 17:30:40 -0500

    Comenzando

--- react-y-typescript/src/App.tsx
+++ react-y-typescript/src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
+
+  const INITIAL_STATE =  [ 
+     {
+       nick: 'dapelu',
+       subMonths: 3,
+       avatar: 'https://i.pravatar.cc/150?u=dapelu',  //un servicio para imagenes de avatars
+       description: 'dapelu do moder'
+     },
+     {
+       nick: 'sergio_serrano',
+       subMonths: 3,
+       avatar: 'https://i.pravatar.cc/150?u=sergio-serrano'
+     }
+   ]
+
+
+//forma pro
+interface Sub{
+  nick: string,
+  subMonths: number,
+  avatar: string,
+  description?: string //Opcional
+}
+
+
 function App() {
+
+
+  const [subs, setSubs] = useState  <Array<Sub>>([]) //forma pro
+
+//Primera renderizacion
+
+useEffect(()=>{
+  setSubs(INITIAL_STATE)
+},[])
+
+
+  //const [subs, setSubs] = useState  ([ Forma manual
+
+    //una forma manual de hacerlo
+  //   {
+  //     nick: 'dapelu',
+  //     subMonths: 3,
+  //     avatar: 'https://i.pravatar.cc/150?u=dapelu',  //un servicio para imagenes de avatars
+  //     description: 'dapelu do moder'
+  //   },
+  //   {
+  //     nick: 'sergio_serrano',
+  //     subMonths: 3,
+  //     avatar: 'https://i.pravatar.cc/150?u=sergio-serrano'
+  //   }
+  // ])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>Dex sub</h1>
+    <ul>
+      {
+        subs.map(subs => {
+          return(
+            <li key={subs.nick}>
+              <img src={subs.avatar} alt={`Avatar for ${subs.nick}`}  />
+              <h4>{subs.nick} (<small>{subs.subMonths}</small>)</h4>
+              <p>{subs.description?.substring(0,100)}</p>
+            </li>
+          )
+        })
+      }
+    </ul>
+
     </div>
   );
 }
