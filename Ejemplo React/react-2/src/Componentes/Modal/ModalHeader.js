bbc430bb71d475e63192e36e49a97e64d63b542f
DexKev6 <kevin_saldarriaga82191@elpoli.edu.co>
2023-04-18 20:59:43 -0500

    cambiar modak

--- Ejemplo React/react-2/src/Componentes/Modal/ModalHeader.js
+++ Ejemplo React/react-2/src/Componentes/Modal/ModalHeader.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./ModalHeader.css"
-function ModalHeader(){
+function ModalHeader({cambiarModal} = () => {}){
     return(
         <div className="modal-header">
         <h1 className="modal-title fs-5" id="exampleModalLabel">Nueva mascota</h1>
         <button type="button" 
         className="btn-close" 
-        data-bs-dismiss="modal" aria-label="Close"></button>
+        data-bs-dismiss="modal" 
+        aria-label="Close"
+        onClick={cambiarModal}
+        ></button>
         <span  aria-hidden="true">&times;</span>
     </div>
     );
